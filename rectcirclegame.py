2af0ebd82faf4d0e4ceb4e3e1c75ad768143d64d
willblack2006 <willblac2006@gmail.com>
2022-04-01 11:51:47 -0500

    w

--- rectcirclegame.py
+++ rectcirclegame.py
@@ -1,82 +1,163 @@
-import os, random, time, pygame
-#Initialize pygame, you also have the option to import it as p or pg
-pygame.init()
-
-#Variables for moving
-move=5 #pixels
-# Declare constant variables, list, dictionaries
-#Capitalize to clarify constants
-#Screen size
+import mailbox
+import os, time, random,math
+import pygame as p
+# K_UP                  up circle
+# K_DOWN                down circle
+# K_RIGHT               right circle
+# K_LEFT                left circle
+# K_a                   left square
+# K_d                   right square
+# K_w                   up square
+# K_s                   down square
+# K_SPACE
+#initialize pygame
+p.init()
+#variables, constants, and objects
 WIDTH=700
 HEIGHT=700
-check=True #<-- For while loop
-#Square position and size
-xs=20
-ys=20
-wbox=30
-hbox=30
-# Circle radius
-CRadius=15
-# Circle random start point (making sure the circle doesn't appear partly offscreen)
-xc=random.randint(CRadius,WIDTH-CRadius)
-yc=random.randint(CRadius,HEIGHT-CRadius)
-# create the objects
-#Our screen:
-screen=pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Circle Eats Square")
-# "Rect" stands for the rectangle shape type
-#The measurments go in x position, y position, width, height 
-square=pygame.Rect(xs,ys,wbox,hbox)
+x=100
+y=15
+xc=random.randint(50, WIDTH-50)
+yc=random.randint(50,HEIGHT-50)
+hbox=20  #height if rect
+wbox=20  #width of rect
+rad=10    #radius of the circle
+#inscribe square
+ibox=rad*math.sqrt(2)
+xi= xc-ibox/2
+yi= yc-ibox/2
+inscSq=p.Rect(xi,yi, ibox,ibox)
+bg=p.image.load('bgSmaller.jpg')
+man=p.image.load('standing.png')
 
+manX=0
+manY=HEIGHT-64
+check=True
+manSquare=p.Rect(manX,manY,64,64)
+boulder = p.Rect(230,270,180,345) 
+square=p.Rect(x,y,wbox,hbox)
 
-#Create the screen
-pygame.display.set_mode((WIDTH,HEIGHT))
+JUMP=False
+#create screen
+screen=p.display.set_mode((WIDTH,HEIGHT))
 
-#Define our colors in a dictionary
-colors={'white':[255,255,255], 'red':[255,0,0], 'orange':[255,85,0], 'purple':[48,25,52,],'aqua':[102,193,255], 'pink': [200,3,75], 'black':[0,0,0], 'navy':[5,31,64]}
+#define colors Dictionary dictio={key: values}
+colors={'red':[255,0,0],'white':[255,255,255],'mag':[255,0,255],
+'aqua':[51,153,255],'m':[47,192,229]}
+#calling values from a dictionaty get('key')
+background=colors.get('white')
+cr_color=colors.get('mag')
+#Create a sqaure rand color 
+#sq_color=colors.get('aqua')
+blColor=colors.get('aqua')
+randColor=''
+def changeClr():
+    print(background)
+    global randColor
+    colorCheck=True
+    while colorCheck:
+        randColor=random.choice(list(colors))
+        print("rand Clr = ", randColor)
+        if colors.get(randColor)  == background:
+            print("backgrnd = randclr")
+            randColor=random.choice(list(colors))
+        else:
+            colorCheck=False
+changeClr()
+sq_color=colors.get(randColor)    
 
-#Call colors to get colors for our screen and shapes
-background=colors.get('pink')
-s_color=colors.get('navy')
-c_color=colors.get('white')
-
-#Define the circle
-
-
-#make a function for our game
+p.display.set_caption("Circle eats Square")
+jumpCount=7
+COUNT=7
+impact=False
 while check:
-    #Fill the screen and draw the shapes (for testing)
-    screen.fill(background)
-    #Checking for events in the pygame and allow for key inputs
-    #For keys use K_(key value)
-    #arrows for circle and wasd for squares
-    for case in pygame.event.get():
-        if case.type==pygame.QUIT:
+    move=10
+    crash=boulder.colliderect(manSquare)
+    if crash:
+        impact = True
+    else:
+        impact=False
+
+    for case in p.event.get():
+        if case.type == p.QUIT:
             check=False
-    keys=pygame.key.get_pressed() #<-- To check if a key gets pressed (classified as a list), the 'and move' part has to do with creating boundries
-    if keys[pygame.K_a] and square.x>=move:
-        square.x-=move #subtract
-    if keys[pygame.K_d] and square.x<=WIDTH-wbox:
-        square.x+=move
-    if keys[pygame.K_w] and square.y>=move:
-        square.y-=move
-    if keys[pygame.K_s] and square.y<=HEIGHT-hbox:
-        square.y+=move
-    # Circle Movements
-    if keys[pygame.K_LEFT] and xc>=move:
-        xc-=move #subtract
-    if keys[pygame.K_RIGHT] and xc<=WIDTH-CRadius:
-        xc+=move
-    if keys[pygame.K_UP] and yc>=move:
-        yc-=move
-    if keys[pygame.K_DOWN] and yc<=HEIGHT-CRadius:
-        yc+=move
+    keys=p.key.get_pressed() # this is a list
+    print(impact)
+    if case.type==p.MOUSEBUTTONDOWN:
+        mouse_pos=p.mouse.get_pos()
+        print(mouse_pos)
+    #Square Moves
+    if keys[p.K_a] and manX>=move:
+        square.x -= move
+        # if not impact:
+        manX -= move
+        manSquare.x -= move
+    if keys[p.K_d] and manX<WIDTH-wbox:
+        square.x += move
+        if not impact:
+            manX += move
+            manSquare.x += move
+    #JUMP CODE
+    if not JUMP:
+        if keys[p.K_s]:
+            square.y += move
+            if not impact:
+                manY += move
+                manSquare.y += move
+        if keys[p.K_w]:
+            square.y -= move
+            if not impact:
+                manY -= move
+                manSquare.y -= move
+        if keys[p.K_SPACE]:
+            JUMP=True
+    else:
+        if jumpCount >= -COUNT:
+            square.y -= jumpCount*abs(jumpCount)/2
+            manY -= jumpCount*abs(jumpCount)/2
+            manSquare.y -= jumpCount*abs(jumpCount)/2
+            jumpCount -=1
+        else:
+            jumpCount=COUNT
+            JUMP=False
+    
+    #circle moves
+    if keys[p.K_LEFT] and xc > move:
+        xc -= move
+        inscSq.x -= move
+    if keys[p.K_RIGHT] and xc <WIDTH-(rad+move):
+        xc += move
+        inscSq.x += move
+    if keys[p.K_DOWN] and yc <HEIGHT-(rad+move):
+        yc += move
+        inscSq.y += move
+    if keys[p.K_UP] and yc > move:
+        yc -= move
+        inscSq.y -= move
 
+   # choque=square.collidepoint((xc,yc))
+    choque=square.colliderect(inscSq )
+    if choque:
+        square.x=random.randint(50, WIDTH-50)
+        square.y=random.randint(50,HEIGHT-50)
+        changeClr()
+        sq_color=colors.get(randColor)    
 
-    pygame.draw.rect(screen,s_color,square)
-    pygame.draw.circle(screen,c_color,(xc,yc),CRadius)
+        rad +=move
+        ibox=rad*math.sqrt(2)
+        xi= xc-ibox/2
+        yi= yc-ibox/2
+        inscSq=p.Rect(xi,yi, ibox,ibox)
+    #Check clide between the man and the bulder
+    
+    
 
-    #Display the screen and shapes via updating (for testing)
-    pygame.display.update()
-    #Add a delay so that we can see our shapes (for testing)
-    pygame.time.delay(10)
+    p.draw.rect(screen,1,manSquare)
+    p.draw.rect(screen,blColor, boulder)
+    screen.blit(bg,(0,0))
+    screen.blit(man,(manX,manY))
+    p.draw.rect(screen,sq_color, square)
+    p.draw.rect(screen,cr_color,inscSq)
+    p.draw.circle(screen,cr_color,(xc,yc), rad )
+    p.display.update()
+    p.time.delay(30)
